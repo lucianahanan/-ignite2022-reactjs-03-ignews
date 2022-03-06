e2bcea7cb0fa99451063ee6c090aa8aed2e79974
Luciana is YammYcoding@freenode:6667 <luphp@yahoo.com.br>
2022-03-06 00:20:02 -0300

    :tada: boilerplate nextjs com typescript e sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>ignews</title>
+    </Head>    
+    <h1>vermelho <span>azul</span></h1>    
+    </>
+  )
+}
